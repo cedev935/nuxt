d6aa4d6c93b2b45bf9af7dcb7d82bc4baff6f6a7
pooya parsa <pooya@pi0.ir>
2020-04-29 11:46:53 +0200

    chore: add playground

---
+++ playground/pages/about.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <p>About</p>
+    <NuxtLink to="/">
+      Home page
+    </NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: process.static ? 'static' : (process.server ? 'server' : 'client')
+    }
+  },
+
+  async setup () {
+    await new Promise(resolve => setTimeout(resolve, process.server ? 100 : 500))
+    return {}
+  },
+
+  head: {
+    title: 'About page'
+  }
+}
+</script>
+
+<style scoped>
+  div {
+    color: 'red'
+  }
+</style>
---
+++ playground/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>Home</p>
+    <NuxtLink to="/about">
+      About Page
+    </NuxtLink>
+    <NuxtLink to="/hello">
+      Hello Page
+    </NuxtLink>
+    <br>
+    <button @click="n++">
+      Increment {{ n }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({ n: 0 }),
+  head: {
+    title: 'Home page'
+  }
+
+}
+</script>
